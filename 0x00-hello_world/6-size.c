50deae98a8119aab39246b7a682de164626f4e07
wezzoooo <mohamed.elganzore@gmail.com>
2023-07-13 18:19:36 -0700

    c is fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
+ * main -Entry point
  *
- * Description : A c program prints the size of data type using sizeof method
+ *Description : A C programm that print the type of variables
  *
- * Return : Always 0 (success)
-*/
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-	printf("Size of a char: %u byte(s)\n", sizeof(char));
-	printf("Size of an int: %u byte(s)\n", sizeof(int));
-	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %u byte(s)\n", sizeof(float));
-	return (0);
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
 }
