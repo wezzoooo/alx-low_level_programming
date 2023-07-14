2bd6c55659c9f4005479af237b087496da119812
wezzoooo <mohamed.elganzore@gmail.com>
2023-07-13 17:39:28 -0700

    c is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+	
+/**
+ * main - Entry point
+ *
+ * Dscription : A c program prints put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+/**
+ * main - Entry point
+ *
+ * Description : A c program prints with printf function
+ *
+ * Return : Always 0 (success)
+ */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
