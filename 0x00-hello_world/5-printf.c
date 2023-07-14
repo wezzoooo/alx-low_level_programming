f73d53cad4cfaddcd7b8dc84b5c30a0056ab89b6
wezzoooo <mohamed.elganzore@gmail.com>
2023-07-13 18:07:26 -0700

    c is fun

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,13 @@
 /**
  * main - Entry point
  *
- * Description : A c program prints with printf function
+ * Description : A C programm that print printf function
  *
  * Return : Always 0 (success)
 */
 
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
