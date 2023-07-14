de07255c4e9ce5798da2c49375d8676b1407c255
wezzoooo <mohamed.elganzore@gmail.com>
2023-07-13 17:54:02 -0700

    c is fun

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-int main(void){
+
 /**
  * main - Entry point
  *
  * Description : A c program prints with printf function
  *
  * Return : Always 0 (success)
- */
+*/
+
+int main(void){
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,8 +5,9 @@
  * Description : A c program prints the size of data type using sizeof method
  *
  * Return : Always 0 (success)
- */
-int main(void){
+*/
+int main(void)
+{
 	printf("Size of a char: %u byte(s)\n", sizeof(char));
 	printf("Size of an int: %u byte(s)\n", sizeof(int));
 	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
