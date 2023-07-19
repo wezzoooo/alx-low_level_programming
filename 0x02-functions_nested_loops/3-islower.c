636fb24f97ff5dd44c0cbbd97acb727840fb679a
wezzoooo <mohamed.elganzore@gmail.com>
2023-07-19 08:42:09 -0700

    for experience

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-*
-*
-*
+*_islower -function takes a char and return output if lower or not.
+*@c: the char that we want to test
+*Return: 1 or 0 dependes lower or not
 */
 
 int _islower(int c)
