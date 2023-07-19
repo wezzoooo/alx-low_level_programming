98f34daf07727c95c4ae914f42cf7aa16f2e00c4
wezzoooo <mohamed.elganzore@gmail.com>
2023-07-19 08:25:03 -0700

    for experience

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabet letters 10 times followed by a new line.
+*Return: void
+*
+*/
+
+void print_alphabet_x10(void)
+{
+int i, l:
+
+for (i = 10; i <= 10; i++)
+{
+	for (l = 'a' ;l <= 'z' ;l++)
+		{
+			_putchar(l);
+		}
+	_putchar(10);
+}
+}
