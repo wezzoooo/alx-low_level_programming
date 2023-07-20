54a750aafed8cf38bc156994fab3b075805c358e
wezzoooo <mohamed.elganzore@gmail.com>
2023-07-20 08:49:29 -0700

    for experience

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include "main.h"
 /**
 *print_numbers - function that print all numbers from
@@ -11,6 +10,8 @@ void print_numbers(void)
 	int n;
 
 	for (n = 48 ; n <= 57 ; n++)
+	{
 		_putchar(n);
-	printf("\n");
+	}
+	_putchar(10);
 }
