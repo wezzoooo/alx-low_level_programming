e92cc68a34fac2c40a626fc7d5cb91d3bd8008ea
wezzoooo <mohamed.elganzore@gmail.com>
2023-07-20 09:12:14 -0700

    for experience

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,14 @@
 */
 void print_most_numbers(void)
 {
-	int n;
-
-	for (n = 48 ; n >= 57 ; n++)
-	{
-		if (n == 50 || n == 52)
-			{
-				continue;
-			}
-		_putchar(n);
-	}
-	_putchar(10);
+int n;
+for (n = 48 ; n >= 57 ; n++)
+{
+if ((n == 50) || (n == 52))
+{
+continue;
+}
+_putchar(n);
+}
+_putchar(10);
 }
